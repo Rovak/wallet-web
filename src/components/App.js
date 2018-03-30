37d6ac3aa592bda4c224b9672d72f0b87be3338b
Roy van Kaathoven <ik@royvankaathoven.nl>
2018-03-30 21:07:07 +0200

    add basename

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ class App extends Component {
 
     return (
       <Provider store={store}>
-        <Router>
+        <Router basename="/wallet-web">
           <React.Fragment>
             <Navigation/>
             <Content/>
