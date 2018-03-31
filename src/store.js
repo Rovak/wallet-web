4b781fa2c9a315aab21ecc107c217fdd757b9e23
Roy van Kaathoven <ik@royvankaathoven.nl>
2018-03-31 13:16:13 +0200

    disable dev tools

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ import reducer from './reducers';
 export function configureStore() {
   const enhancer = compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
   return createStore(reducer, enhancer);
