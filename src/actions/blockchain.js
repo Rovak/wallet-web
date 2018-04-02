8782e1b8feb68f8ce2d4680e2fc236e7a425bddd
Roy van Kaathoven <ik@royvankaathoven.nl>
2018-04-02 17:55:42 +0200

    skip numbers below 0

--- src/actions/blockchain.js
+++ src/actions/blockchain.js
@@ -26,6 +26,9 @@ export const loadBlocks = () => async (dispatch, getState) => {
   let blocks = [];
 
   for (let i = 0; i < 7; i++) {
+    if ((block.number - i) <= 0) {
+      break;
+    }
     blocks.push(await Client.getBlockByNum(block.number - i));
   }
 
