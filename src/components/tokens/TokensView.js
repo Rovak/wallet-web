6284149a5b27ebe5d5d365562b509cac92e3b985
john <physitime@gmail.com>
2018-04-12 17:15:49 +0430

    add tu function

--- src/components/tokens/TokensView.js
+++ src/components/tokens/TokensView.js
@@ -156,7 +156,7 @@ class TokensView extends Component {
                     <tr>
                       <td colSpan="5">
                         <div className="alert alert-warning text-center">
-                          An error occurred
+                         {tu("An_error_occurred")}
                         </div>
                       </td>
                     </tr>
