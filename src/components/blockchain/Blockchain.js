1096d01d485811ccc328b4966bcf6d2fb050d412
AhnSinYong <ahnsy29@gmail.com>
2018-04-14 00:30:05 +0900

    add unique key prop

--- src/components/blockchain/Blockchain.js
+++ src/components/blockchain/Blockchain.js
@@ -92,8 +92,8 @@ class Blockchain extends Component {
     return (
       <Fragment>
         {
-          transactions.slice(0, 7).map(transaction => (
-            <div className="media text-muted pt-3">
+          transactions.slice(0, 7).map((transaction, i) => (
+            <div key={i} className="media text-muted pt-3">
               <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" className="mr-2 rounded"/>
               <div className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray text-center">
                 {transaction.from.substr(0, 16)}...
