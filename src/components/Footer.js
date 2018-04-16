dd1296ee1d8a6f6f785361309e6fbea8f0040abe
Jacob Schuster <jschuster8765@outlook.com>
2018-04-16 14:04:27 -0400

    Footer Enhancements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ export default class Footer extends Component {
     return (
       <footer className="container text-center pt-1">
         <a className="mr-1 text-muted" href="https://github.com/tronprotocol/wallet-web" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-github"/>
+          <i className="fab fa-github social mr-2"/>
         </a>
         <a className="mr-1 text-muted" href="https://twitter.com/tronfoundation" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-twitter"/>
+          <i className="fab fa-twitter social ml-2"/>
         </a>
       </footer>
     )
