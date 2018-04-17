97d79244eb45890ad2a1084be1bf3503db9f283c
Jacob Schuster <jschuster8765@outlook.com>
2018-04-17 13:45:47 -0400

    Fix translation

--- src/components/common/ExternalUrl.js
+++ src/components/common/ExternalUrl.js
@@ -11,7 +11,7 @@ class ExternalUrl extends Component {
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="ExternalUrlWarningLabel">External URL</h5>
+                <h5 class="modal-title" id="ExternalUrlWarningLabel">{tu("External URL")}</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
