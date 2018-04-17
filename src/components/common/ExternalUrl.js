ce339dddd55cc9cf770c873cc80e73fb51351895
Jacob Schuster <jschuster8765@outlook.com>
2018-04-16 23:31:26 -0400

    Typo and code style

--- src/components/common/ExternalUrl.js
+++ src/components/common/ExternalUrl.js
@@ -7,7 +7,7 @@ class ExternalUrl extends Component {
       <div>
         <a href="#" data-toggle="modal" data-target="#ExternalUrlWarning">{this.props.url}</a>
 
-        <div class="modal fade" id="ExternalUrlWarning" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="ExternalUrlWarning" tabindex="-1" role="dialog" aria-labelledby="ExternalUrlWarning" aria-hidden="true">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
